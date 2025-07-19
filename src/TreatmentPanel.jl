5d6e6f90f3d460b2a33b2612071f275c40a73cc2
Daniel Prosi <69299070+ProsiDecon@users.noreply.github.com>
2025-07-19 17:49:07 +0200

    introduce BalancedPanelQ structs and their functions to deal with baseline weights

--- src/TreatmentPanel.jl
+++ src/TreatmentPanel.jl
@@ -214,6 +214,22 @@ function treated_ids(x::BalancedPanel{MultiUnitTreatment{T}}) where T
     any.(eachrow(x.W))
 end
 
+
+function treated_ids(x::BalancedPanelQ{SingleUnitTreatment{T}}) where T
+    for i ∈ 1:size(x.Y, 1)
+        for t ∈ 1:size(x.Y, 2)
+            if x.W[i, t]
+                return i
+            end
+        end
+    end
+end
+
+function treated_ids(x::BalancedPanelQ{MultiUnitTreatment{T}}) where T
+    any.(eachrow(x.W))
+end
+
+
 """
     treated_labels(x <: BalancedPanel)
 
@@ -224,6 +240,14 @@ function treated_labels(x::BalancedPanel{SingleUnitTreatment{T}}) where T
 end
 
 function treated_labels(x::BalancedPanel{MultiUnitTreatment{Simultaneous{Continuous}}})
+    x.is[treated_ids(x)]
+end
+
+function treated_labels(x::BalancedPanelQ{SingleUnitTreatment{T}}) where T
+    x.is[treated_ids(x)]
+end
+
+function treated_labels(x::BalancedPanelQ{MultiUnitTreatment{Simultaneous{Continuous}}})
     x.is[treated_ids(x)]
 end
 
@@ -264,6 +288,14 @@ function length_T₀(x::BalancedPanel{SingleUnitTreatment{Continuous}})
 end
 
 function length_T₀(x::BalancedPanel{MultiUnitTreatment{Simultaneous{Continuous}}})
+    first_treated_period_ids(x) .- 1
+end
+
+function length_T₀(x::BalancedPanelQ{SingleUnitTreatment{Continuous}})
+    first_treated_period_ids(x) - 1
+end
+
+function length_T₀(x::BalancedPanelQ{MultiUnitTreatment{Simultaneous{Continuous}}})
     first_treated_period_ids(x) .- 1
 end
 
@@ -477,6 +509,264 @@ function BalancedPanel_maker(df::DataFrame, treatment_assignment::Union{Pair{T1,
     BalancedPanel{MultiUnitTreatment{uttype{tdtype}}}(W, Y, df, id_var, t_var, outcome_var, ts, is)  
 end
 
+
+
+###################################################################################################
+### add Balanced Panel structs with baseline weights Q (either a dyadic tensor object or an N × T matrix)
+@with_kw struct BalancedPanelQ{UTType} <: TreatmentPanel where UTType <: TreatmentType
+    W::Union{Matrix{Bool}, Matrix{Union{Missing, Bool}}}
+    #Y::Union{Matrix{Float64},Array{Float64,3}}
+    Y::Array
+    Q::Array                # the baseline weights 
+    df::DataFrame
+    id_var::Union{String, Symbol}
+    t_var::Union{String, Symbol}
+    outcome_var::Union{String, Symbol}
+    ts::Vector{T1} where T1 <: Union{Date, Int64}
+    is::Vector{T2} where T2 <: Union{Symbol, String, Int64}
+end
+
+# Constructor for single continuous treatment - returns BalancedPanel{SingleUnitTreatment{Continuous}}
+function BalancedPanelQ_maker(df::DataFrame, 
+                        treatment_assignment::Pair{<:Any, <:Union{Date, Int}},
+                        df_q::Union{Nothing, DataFrame} = nothing;
+                        id_var = nothing, 
+                        t_var = nothing, 
+                        outcome_var = nothing, 
+                        q_var = nothing,                # the variable name if the weights in either df or df_q
+                        q_id_var = nothing,             # if q_dyadic, the variable name of the (treated) partner of the current weight
+                        q_dyadic::Bool = true,
+                        covariates::Union{Nothing, Vector{Symbol}, Symbol} = nothing,
+                        sort_inplace = false) #where T1 where T2 <: Union{Date, Int}
+
+    if typeof(covariates) == Symbol
+        covariates = [covariates]
+    end
+                        
+    # Get all units and time periods
+    is = sort(unique(df[!, id_var])); i_set = Set(is)
+    ts = sort(unique(df[!, t_var])); t_set = Set(ts)
+
+    # Dimensions
+    N = length(is)
+    T = length(ts)
+
+    # Get all treatment units and treatment periods
+    treated_i = first(treatment_assignment)
+    treated_t = last(treatment_assignment)
+
+    # Sanity checks
+    check_id_t_outcome(df, outcome_var, id_var, t_var)
+    in(treated_i, i_set) || throw("Error: Treatment unit $treated_i is not in the list of unit identifiers $id_var")
+    in(treated_t, t_set) || throw("Error: Treatment period $treated_t is not in the list of time identifiers $t_var")
+    
+    # Sort data if necessary, in place if required
+    df = ifelse(issorted(df, [id_var, t_var]), df, 
+                                               ifelse(sort_inplace, sort!(df, [id_var, t_var]), 
+                                                                    sort(df, [id_var, t_var])))
+
+    # Treatment matrix
+    W = construct_W(treatment_assignment, N, T, is, ts)
+    #W = TreatmentPanels.construct_W(treatment_assignment, N, T, is, ts)
+
+    # Outcome matrix
+    if !isnothing(covariates)
+        Y = zeros(eltype(df[!, outcome_var]), (size(W)..., length(covariates)+1))
+        for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts)
+            Y[row, col,1] = only(df[(df[!, id_var] .== i) .& (df[!, t_var] .== t), outcome_var])
+        end
+        # add covariates in 3rd dimension
+        for covariate in eachindex(covariates)
+            for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts)
+                Y[row, col,1+covariate] = only(df[(df[!, id_var] .== i) .& (df[!, t_var] .== t), covariates[covariate]])
+            end
+        end
+    else
+        Y = zeros(eltype(df[!, outcome_var]), size(W))
+        for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts)
+            Y[row, col] = only(df[(df[!, id_var] .== i) .& (df[!, t_var] .== t), outcome_var])
+        end
+    end
+
+    # make the object for baseline weights
+    if q_dyadic 
+        !isnothing(df_q) || error(ArgumentError("Dyadic weights chosen but no dataframe df_q for weights provided."))
+        !isnothing(q_var) || error(ArgumentError(
+            "Please specify q_var, the name of the column in your dataset holding the weight variable in your weight dataset df_q."))
+        !isnothing(q_id_var) || error(ArgumentError(
+            "Please specify q_id_var, the name of the column in your dataset holding the dyadic partner for the weight in your weight dataset df_q. Otherwise specify q_dyadic = false"))
+
+        Q = zeros(eltype(df_q[!, q_var]), (size(W)..., N))
+
+        q_lookup = Dict{Tuple{eltype(is), eltype(is), eltype(ts)}, eltype(df_q[!, q_var])}()
+
+        for row in eachrow(df_q)
+            key = (row[id_var], row[q_id_var], row[t_var])
+            q_lookup[key] = row[q_var]  # Overwrites duplicates, change if needed
+        end
+
+        # Now loop and fill Q efficiently
+        for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts), (slice, j) ∈ enumerate(is)
+            Q[row, col, slice] = get(q_lookup, (i, j, t), missing)
+        end
+
+        #for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts), (slice, j) ∈ enumerate(is)
+        #    Q[row, col, slice] = only(df_q[(df_q[!, id_var] .== i) .& (df_q[!, q_id_var] .== j) .& (df_q[!, t_var] .== t), q_var])
+        #end
+    else
+        if isnothing(df_q) && !isnothing(q_var)
+            df_q = select(df, [id_var, t_var, q_var])
+        elseif isnothing(df_q) && isnothing(q_var)
+            error(ArgumentError("Neither dataframe for weights df_q provided nor variable q_var for weights specified."))
+        end
+
+        Q = zeros(eltype(df[!, q_var]), (size(W)))
+        for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts)
+            Q[row, col] = only(df_q[(df_q[!, id_var] .== i) .& (df_q[!, t_var] .== t), q_var])
+        end
+    end
+
+    return BalancedPanelQ{SingleUnitTreatment{Continuous}}(W, Y, Q, df, id_var, t_var, outcome_var, ts, is)  
+end
+
+
+
+
+# Fallback method - if the length of treatment assignment is one use single treatment method above
+function BalancedPanelQ_maker(df::DataFrame, treatment_assignment,
+                                df_q::Union{Nothing, DataFrame} = nothing;
+                                id_var = nothing, 
+                                t_var = nothing, 
+                                outcome_var = nothing, 
+                                q_var = nothing,
+                                q_id_var = nothing,
+                                q_dyadic::Bool = true,
+                                covariates::Union{Nothing, Vector{Symbol}, Symbol} = nothing,
+                                sort_inplace = false)
+
+    if length(treatment_assignment) == 1
+        return BalancedPanel_maker(df, only(treatment_assignment); id_var = id_var, t_var = t_var,
+                                outcome_var = outcome_var, covariates = covariates, sort_inplace = sort_inplace)
+    end
+
+    if typeof(covariates) == Symbol
+        covariates = [covariates]
+    end
+
+    # Get all units and time periods
+    is = sort(unique(df[!, id_var])); i_set = Set(is)
+    ts = sort(unique(df[!, t_var])); t_set = Set(ts)
+
+    # Get all treatment units and treatment periods
+    treated_is = first.(treatment_assignment)
+    treated_is = typeof(treated_is) <: AbstractArray ? treated_is : [treated_is]
+    treated_ts = treatment_periods(treatment_assignment)
+    #treated_ts = TreatmentPanels.treatment_periods(treatment_assignment)
+
+    # Dimensions
+    N = length(is)
+    T = length(ts)
+
+    ### SANITY CHECKS ###
+    #TreatmentPanels.check_id_t_outcome(df, outcome_var, id_var, t_var)
+    check_id_t_outcome(df, outcome_var, id_var, t_var)
+    for ti ∈ treated_is
+        in(ti, i_set) || throw("Error: Treatment unit $ti is not in the list of unit identifiers $id_var")
+    end
+
+    for tt ∈ treated_ts
+        in(tt, t_set) || throw("Error: Treatment period $tt is not in the list of time identifiers $t_var")
+    end
+    
+    # Sort data if necessary, in place if required
+    df = ifelse(issorted(df, [id_var, t_var]), df, 
+                                               ifelse(sort_inplace, sort!(df, [id_var, t_var]), 
+                                                                    sort(df, [id_var, t_var])))
+
+    # Treatment matrix
+    W = construct_W(treatment_assignment, N, T, is, ts)
+    #W = TreatmentPanels.construct_W(treatment_assignment, N, T, is, ts)
+    
+    # Outcome matrix
+    if !isnothing(covariates)
+        Y = zeros(eltype(df[!, outcome_var]), (size(W)..., length(covariates)+1))
+        for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts)
+            Y[row, col,1] = only(df[(df[!, id_var] .== i) .& (df[!, t_var] .== t), outcome_var])
+        end
+        # add covariates in 3rd dimension
+        for covariate in eachindex(covariates)
+            for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts)
+                Y[row, col,1+covariate] = only(df[(df[!, id_var] .== i) .& (df[!, t_var] .== t), covariates[covariate]])
+            end
+        end
+    else
+        Y = zeros(eltype(df[!, outcome_var]), size(W))
+        for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts)
+            Y[row, col] = only(df[(df[!, id_var] .== i) .& (df[!, t_var] .== t), outcome_var])
+        end
+    end
+
+    # make the object for baseline weights
+    if q_dyadic 
+        !isnothing(df_q) || error(ArgumentError("Dyadic weights chosen but no dataframe df_q for weights provided."))
+        !isnothing(q_var) || error(ArgumentError(
+            "Please specify q_var, the name of the column in your dataset holding the weight variable in your weight dataset df_q."))
+        !isnothing(q_id_var) || error(ArgumentError(
+            "Please specify q_id_var, the name of the column in your dataset holding the dyadic partner for the weight in your weight dataset df_q. Otherwise specify q_dyadic = false"))
+
+        Q = zeros(eltype(df_q[!, q_var]), (size(W)..., N))
+
+        q_lookup = Dict{Tuple{eltype(is), eltype(is), eltype(ts)}, eltype(df_q[!, q_var])}()
+
+        for row in eachrow(df_q)
+            key = (row[id_var], row[q_id_var], row[t_var])
+            q_lookup[key] = row[q_var]  # Overwrites duplicates, change if needed
+        end
+
+        # Now loop and fill Q efficiently
+        for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts), (slice, j) ∈ enumerate(is)
+            Q[row, col, slice] = get(q_lookup, (i, j, t), missing)
+        end
+
+        #for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts), (slice, j) ∈ enumerate(is)
+        #    Q[row, col, slice] = only(df_q[(df_q[!, id_var] .== i) .& (df_q[!, q_id_var] .== j) .& (df_q[!, t_var] .== t), q_var])
+        #end
+    else
+        if isnothing(df_q) && !isnothing(q_var)
+            df_q = select(df, [id_var, t_var, q_var])
+        elseif isnothing(df_q) && isnothing(q_var)
+            error(ArgumentError("Neither dataframe for weights df_q provided nor variable q_var for weights specified."))
+        end
+
+        Q = zeros(eltype(df[!, q_var]), (size(W)))
+        for (row, i) ∈ enumerate(is), (col, t) ∈ enumerate(ts)
+            Q[row, col] = only(df_q[(df_q[!, id_var] .== i) .& (df_q[!, t_var] .== t), q_var])
+        end
+    end
+
+    # Determine TreatmentType and TreatmentDurationType
+    uttype = if all(==(treatment_assignment[1][2]), last.(treatment_assignment))
+        Simultaneous
+    else
+        Staggered
+    end
+
+    tdtype = if typeof(treatment_assignment) <: Pair
+        if typeof(treatment_assignment[2]) <: Pair
+            Discontinuous
+        else
+            Continuous
+        end
+    else
+        if typeof(treatment_assignment[1][2]) <: Pair
+            Discontinuous
+        else
+            Continuous
+        end
+    end
+
+    BalancedPanelQ{MultiUnitTreatment{uttype{tdtype}}}(W, Y, Q, df, id_var, t_var, outcome_var, ts, is)  
+end
 
 
 ## UnblancedPanel - N observations but not all of them for T periods
